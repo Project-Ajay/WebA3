18bfc35092a507b1b2d6d28f94f3fa7dcabd0e49
Ajay Persaud <persauaj@sheridancollege.ca>
2024-03-29 11:25:00 -0400

    create Header,Footer component (1) added all the info and create the html to use in app.html

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  componentType:string = "Header";
+  project:string = "Assignment 3";
+  currentData : Date = new Date();
+
 
 }
