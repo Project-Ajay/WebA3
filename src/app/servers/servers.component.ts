b542724532eca47586028fdd2d2dd829dd69b259
Ajay Persaud <persauaj@sheridancollege.ca>
2024-03-29 14:23:49 -0400

    Created the Server Form and implement the list view to show the created addded servers

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import  {Server} from "../server";
+import {FormsModule, NgForm} from "@angular/forms";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-servers',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgForOf
+  ],
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
+  servers: Server[] = [];
+  serverName: string = " "; // Property to hold the server name from the form
+  serverId: string = " "; // Property to hold the server ID from the form
+  serverStatus: string = " "; // Property to hold the server status from the form
 
+  addServer(form: NgForm) {
+    const newServer = new Server(this.serverName,this.serverStatus,this.serverId);
+    this.servers.push(newServer);
+    form.resetForm();
+  }
 }
